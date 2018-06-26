b504c628d9fecdf3d92286a6e0f106e7fc3ef480
Saravanan Kandaswamy <ksnblr@gmail.com>
2018-06-26 13:21:47 -0400

    app.py changes

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ app.debug = True
 
 func_filename = "Q:\\qa\\work_areas\\xkk\\do_not_delete\\workspace\\fun_dash\\files\\function_data.xlsx"
 print ("Last Refresh ... {}" .format(time.ctime(os.path.getmtime(func_filename))))
-ref = time.ctime(os.path.getmtime(func_filename))
 
 df_out = read_dataframe(func_filename)
 new_json_data = df_out[0]
@@ -19,6 +18,7 @@ pd_columns = df_out[1]
 
 @app.route('/')
 def dataIndex():
+    ref = time.ctime(os.path.getmtime(func_filename))
     return render_template('data.html', columns=pd_columns, lst_ref =ref)
 
 @app.route('/fun_data')
